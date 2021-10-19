cca790431638c83b5595ff00e6d73c7300e5bc4d
joel <>
2021-10-19 18:00:10 -0500

    BookingDone

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {
+  BrowserRouter as Router, 
+  Switch,
+  Route,
+  link
+} from "react-router-dom"; 
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import NavBar from "./NavBar"; 
+import Booking from "./Booking";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <NavBar/> */}
+      <Booking/>
     </div>
   );
 }
